d057e994c90e61a34f3672bad2a505b05504aa0f
tiagodeveloper <tiagopereirafonseca@gmail.com>
2019-06-10 21:39:42 -0300

    master: Arquitetura e testes com o evento de click.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Map, tileLayer, marker } from 'leaflet';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  map: Map;
+  latLong: Array<number> = [];
+  maker:any;
 
-  constructor() {}
+  constructor() {
+    this.latLong.push(-19.774414);
+    this.latLong.push(-43.983566);
+  }
+
+  loadmap() {
+    this.map = new Map('mapId').setView(this.latLong, 15);
+    tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18
+    }).addTo(this.map);
+
+    this.map.on('click', function(e) {
+      
+      if(this.maker)
+        e.target.removeLayer(this.maker);
+
+      this.maker = marker([e.latlng.lat,e.latlng.lng]).addTo(e.target)
+      .bindPopup('Lat e lng: '+ e.latlng)
+      .openPopup();
+    });
+
+  }
+
+  ionViewDidEnter() { 
+    this.loadmap();
+  }
+
+  ionViewWillLeave() {
+    this.map.remove();
+  }
 
 }
